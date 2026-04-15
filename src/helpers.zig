1bfb9f976bd0cd056229e917b5c8662ba35a533b
keeper <97927259+Supraboy981322@users.noreply.github.com>
2026-04-15 17:54:47 -0500

    helper to write to a file or return false

--- src/helpers.zig
+++ src/helpers.zig
@@ -12,3 +12,33 @@ pub fn itr(re:*std.Io.Reader, line_start:?*usize, i:?*usize) !?u8 {
     };
     return byte;
 }
+
+pub fn write_or_err_and_break(
+    file:*std.fs.File,
+    msg:[]u8,
+    comptime msg_type:[]const u8,
+    comptime opts:struct{
+        newline:bool = false,
+    },
+) !bool {
+    const n = try file.write(msg);
+
+    var ok = if (n != msg.len) b: {
+        std.debug.print(
+            msg_type ++ " expected to write {d} bytes, but only wrote {d}\n",
+            .{ msg.len, n }
+        );
+        break :b true;
+    } else
+        false;
+
+    if (!ok) return false; //write to file or err and break
+
+    if (opts.newline) {
+        comptime var new_opts = opts;
+        new_opts.newline = false;
+        ok = try write_or_err_and_break(file, @constCast("\n"), msg_type, new_opts);
+    }
+
+    return ok;
+}
